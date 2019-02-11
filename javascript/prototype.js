c9420320ca9ffcd98ff6e5e9b26fb112e6236e32
forgetIt <2271404280@qq.com>
2019-02-11 16:03:50 +0800

    123

--- javascript/prototype.js
+++ javascript/prototype.js
@@ -8,7 +8,7 @@ function Cls() {
     var a = 'a'; // 私有变量(外部无法访问)
     this.c1 = 'c'; // 实例变量(对象无法访问)
     this.c2 = [];
-    this.printStr1 = function() {
+    this.print1 = function() {
         console.log(this.c1, this.c2);
     }
 }
@@ -29,20 +29,18 @@ console.log(instance1.c1, instance2.c1); // 'cc' 'c'
 console.log(instance1.c2, instance2.c2); // [] [ 'hello' ]
 console.log();
 /**
- * 每个实例都要保持一份方法的复制，这显然是不科学的
+ * 每个实例都要保持一份方法的复制，这是不科学的
  */
-Cls.prototype.printStr2 = function() {
-    console.log(this.c1, this.c2);
-}
-instance1.printStr1(); // cc []
-instance2.printStr1(); // c [ 'hello' ]
-console.log(instance1.printStr1 === instance2.printStr1); // false
-instance1.printStr2(); // cc []
-instance2.printStr2(); // c [ 'hello' ]
-console.log(instance1.printStr2 === instance2.printStr2); // true
+Cls.prototype.print2 = function() { console.log(this.c1, this.c2); };
+instance1.print1(); // cc []
+instance2.print1(); // c [ 'hello' ]
+console.log(instance1.print1 === instance2.print1); // false
+instance1.print2(); // cc []
+instance2.print2(); // c [ 'hello' ]
+console.log(instance1.print2 === instance2.print2); // true
 console.log();
 /**
- * 原型链
+ * @原型链
  * 普通对象(最下级实例对象)没有 propotype ，但是有 __proto__ 属性
  * __propo__ 指向用于创建自身的原型对象的 prototype
  * 实例-->对象原型--> object --> null 之间有一条链式关系，所有 JS 对象都是一级一级克隆下来的
@@ -54,3 +52,35 @@ console.log();
 console.log(instance1.prototype,  instance1 instanceof Cls); // undefined true
 console.log(instance1.__proto__ === Cls.prototype); // true
 console.log(Cls.__proto__ === Function.prototype); // true
+console.log();
+/**
+ * @多继承
+ * 把方法拿来给对象用(对象可以通过继承拥有任意对象的任意方法)
+ * func.call(obj, arg, ...)
+ * func.apply(obj, [arg, ...])
+ */
+function add(a, b) { console.log(a + b); };
+function sub(a, b) { console.log(a - b); };
+add.call(sub, 4, 2); // 6
+
+function A() {
+    this.a = 123;
+    this.print = function() { console.log(this.a); };
+}
+
+function B() {
+    this.a = 456;
+    new A().print.call(this);
+}
+new B(); // 456
+
+function C() {
+    this.a = 456;
+}
+new A().print.call(new C()); // 456
+
+function D() {
+    A.call(this);
+    this.a = 456;　// 注意
+}
+new D().print(); // 456
